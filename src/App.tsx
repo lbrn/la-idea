4ba93afd4dfaec48742915b97258f858e34cf1c6
Joshua <joshuatanderson1@gmail.com>
2020-05-09 11:37:40 -0500

    coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import theme from './theme/theme';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+        <p style={{color:`${theme.palette.primary.main}`}}>
           LA IDeA website: coming soon
         </p>
       </header>
---
+++ src/theme/theme.ts
@@ -0,0 +1,12 @@
+
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+		main: "#AF170B"
+	}
+  },
+});
+
+export default theme;
