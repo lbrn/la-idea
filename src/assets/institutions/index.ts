abaf8a1d462996e5f1247303293fccbab070bc4c
Joshua Anderson <joshuatanderson1@gmail.com>
2020-07-08 21:57:05 -0500

    added lsuhs

--- src/assets/institutions/index.ts
+++ src/assets/institutions/index.ts
@@ -2,6 +2,7 @@ export const institutionImages: { [institution: string]: string } = {
 	tulane: require('./tulane.png'),
 	lsu: require('./lsu.png'),
 	lsuhsc: require('./lsuhsc.png'),
+	lsuhs: require('./lsuhs.png'),
 	pennington: require('./pennington.png'),
 	ulm: require('./ulm.png'),
 };
