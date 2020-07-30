291f9e9fbd6e3dcbbd4829037d058930984b5354
Joshua Anderson <joshuatanderson1@gmail.com>
2020-07-29 22:46:31 -0500

    fix(header): menu alignment works! :D :D :D

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { AppBar, Toolbar, makeStyles } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { AppBar, Toolbar, makeStyles, Grid, Button } from '@material-ui/core';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Link } from 'react-router-dom';
 
 import MainMenu from './MainMenu';
@@ -22,18 +23,59 @@ const useStyles = makeStyles({
 		backgroundRepeat: 'no-repeat',
 		width: 160,
 	},
+	link: {
+		color: theme.palette.primary.main,
+	},
+	toolbar: {
+		display: 'flex',
+		justifyContent: 'flex-start',
+		justifyItems: 'space-between',
+	},
+	linksCont: {
+		marginLeft: "auto"
+	}
 });
+
+// btns --> pullRight
 
 const Header = () => {
 	const classes = useStyles();
 
 	return (
 		<AppBar position="static" className={classes.appBar}>
-			<Toolbar>
+			<Toolbar className={classes.toolbar}>
 				<MainMenu />
 				<Link to="/">
 					<div className={classes.logo} />
 				</Link>
+
+				{useMediaQuery(theme.breakpoints.up('sm')) && (
+					<div className={classes.linksCont}>
+						<Button component={Link} className={classes.link} to="/About">
+							About
+						</Button>
+						<Button
+							component={Link}
+							className={classes.link}
+							to="/Investigators"
+						>
+							Investigators
+						</Button>
+						<Button
+							component={Link}
+							className={classes.link}
+							to="/Institutions"
+						>
+							Institutions
+						</Button>
+						<Button component={Link} className={classes.link} to="/Awards">
+							Awards
+						</Button>
+						<Button component={Link} className={classes.link} to="/Resources">
+							Resources
+						</Button>
+					</div>
+				)}
 			</Toolbar>
 		</AppBar>
 	);
